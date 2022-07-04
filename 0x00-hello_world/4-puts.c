fba07b0c25238e5d0e34348309407ce6c1a5540b
sirak78 <cpu.sirak@gmail.com>
2022-07-04 06:08:20 -0700

    c programming hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+puts("\"Programming is like building mulitlingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is apice of art.n\");
+	return (0);
+}
